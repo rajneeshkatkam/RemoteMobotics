e43c4a4efe456ab14bbc81ab3309f4426f3d3b73
Rajneesh Katkam <rajneesh.katkam@gmail.com>
2018-07-03 18:03:30 +0530

    sending KpKiKd Values to the Hotspot device added

--- app/src/main/java/com/raj/remotemobotics/MainActivity.java
+++ app/src/main/java/com/raj/remotemobotics/MainActivity.java
@@ -1,5 +1,8 @@
 package com.raj.remotemobotics;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.illposed.osc.OSCListener;
 import com.illposed.osc.OSCMessage;
@@ -26,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     Button openCamButton,kpSend,kiSend,kdSend;
     EditText KpInput,KiInput,KdInput;
 
-    Handler handler;
 
     private TextView yaw,pitch,roll,statusTextView;
 
@@ -37,12 +40,13 @@ public class MainActivity extends AppCompatActivity {
     private int hotspot_Port= 1111;        ///Remote device Listening port
 
     private OSCPortOut sender;
-    private OSCPortIn receiver;
+    private OSCPortIn receiver,receiverpid;
 
 
     Boolean sendPIDFlag=false;
 
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,6 +74,18 @@ public class MainActivity extends AppCompatActivity {
         roll= (TextView) findViewById(R.id.roll);
 
 
+        openCamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(),CameraSettingsActivity.class));
+
+            }
+        });
+
+
+
+
 
     }
 
@@ -78,13 +94,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendPIDValues(View view) {
 
-        Thread thread = new Thread() {
+        Toast.makeText(this,String.valueOf(view.getId()),Toast.LENGTH_SHORT).show();
+
+        sendPacketsToHotSpotDevice();
+        /*Thread thread = new Thread() {
             @Override
             public void run() {
                 sendPacketsToHotSpotDevice();
             }
         };
         thread.start();
+        Toast.makeText(this,"send",Toast.LENGTH_SHORT).show();
+*/
 
     }
     ///-------------------------------------------------------------------------------------------
@@ -94,35 +115,47 @@ public class MainActivity extends AppCompatActivity {
 
     private void sendPacketsToHotSpotDevice() {
 
-        Log.i("portError","Packet Called");
+        Thread thread=new Thread() {
+            @Override
+            public void run() {
 
-        if (sender != null) {
-            try {
+                Log.i("portError","Packet Called");
 
-                Log.i("portError","Packet in");
+                if (sender != null) {
+                    try {
 
-                ///////Send Messages with arguments a multiple of 2------Very Important
+                        Log.i("portError","Packet in");
 
-                OSCMessage message = new OSCMessage("/motorValues");
-                message.addArgument(Float.valueOf(String.valueOf(KpInput.getText())));
-                message.addArgument(Float.valueOf(String.valueOf(KiInput.getText())));
-                message.addArgument(Float.valueOf(String.valueOf(KdInput.getText())));
-                message.addArgument(2);
-                //message.addArgument(roll);
-                //Log.i("portError","Packet Created");
+                        ///////Send Messages with arguments a multiple of 2------Very Important
+
+                        OSCMessage message = new OSCMessage("/PIDValues");
+                        message.addArgument(Float.valueOf(String.valueOf(KpInput.getText())));
+                        message.addArgument(Float.valueOf(String.valueOf(KiInput.getText())));
+                        message.addArgument(Float.valueOf(String.valueOf(KdInput.getText())));
+                        message.addArgument(2);
+                        //message.addArgument(roll);
+                        //Log.i("portError","Packet Created");
 
 
-                sender.send(message);
-                Log.i("portError","Packet Sent");
+                        sender.send(message);
+                        Log.i("portError","Packet Sent");
 
-            } catch (Exception e) {
+                    } catch (Exception e) {
 
-                Log.i("portError",e.toString());
-                // Error handling for some error
+                        Log.i("portError",e.toString());
+                        // Error handling for some error
+                    }
+                }
+
+
             }
-        }
+        };
+
+        thread.start();
 
     }
+
+
 
 
     @Override
@@ -135,6 +168,8 @@ public class MainActivity extends AppCompatActivity {
         try {
             // Connect to some IP address and port
             sender = new OSCPortOut(InetAddress.getByName(hotspot_IP), hotspot_Port);
+            Log.i("portErrorSender","Sucess");
+
         } catch (Exception e) {
             // Error handling for any other errors
             Log.i("portError",e.getMessage());
@@ -143,8 +178,31 @@ public class MainActivity extends AppCompatActivity {
 
         ///For receiving messages from Hotspot
         try {
+
              receiver = new OSCPortIn(remoteDevice_Port);  ///Listening port number
-            Log.i("portError","Sucess");
+             //receiverpid = new OSCPortIn(remoteDevice_Port);  ///Listening port number
+
+
+            ////PID Values Listener
+            OSCListener pidListener = new OSCListener() {
+                public void acceptMessage(Date time, OSCMessage message) {
+
+                    List<Object> args = message.getArguments();
+
+                    //Log.i("ArraySize",message.getArguments().toString());
+                    KpInput.setText(String.valueOf(args.get(0)));
+                    KiInput.setText(String.valueOf(args.get(1)));
+                    KdInput.setText(String.valueOf(args.get(2)));
+
+                    ///received message
+                }
+            };
+            receiver.addListener("/KpKiKdValues", pidListener); //////Listening for Tag = '/test'
+           // receiverpid.startListening();
+
+
+
+            ////Sensor Values Listener
             OSCListener listener = new OSCListener() {
                 public void acceptMessage(Date time, OSCMessage message) {
 
@@ -166,13 +224,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         receiver.close();
+        receiverpid.close();
     }
 
+
+
 }
+
+
